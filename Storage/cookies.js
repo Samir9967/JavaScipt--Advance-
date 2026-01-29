f1d708f1d6b713ce00e55e94d6385e7b5196c73a
user.email : "maharanasamir12@gmail.com <user.email:maharanasamir12@gmail.com>
2026-01-29 16:53:40 +0530

    Add cookies part

--- Storage/cookies.js
+++ Storage/cookies.js
@@ -1,3 +1,6 @@
 // cookies -> browser me data store karta hai and apka data browser ke cookies naam ki property mein save hota hai, and cookies concept me kam data ya light data ke liye hota hai
 
 // cookies me 4kb ka data store hota hai
+
+document.cookie = "name = Samir Maharana"; 
+document.cookie = "age = 27";
